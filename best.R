51480cffe0c0eff4101f890a794f2d1b6eddcf07
Kole Huang <yjhuang_18@hotmail.com>
2014-06-18 10:23:18 -0400

    Finalized best function

---
+++ best.R
@@ -0,0 +1,29 @@
+best <- function(state, outcome) {
+
+	# invalid outcome 
+	if (outcome == "heart attack") { 
+		outcome.index <- 11
+	} else if (outcome == "heart failure") {
+		outcome.index <- 17
+	} else if (outcome == "pneumonia") {
+		outcome.index <- 23
+	} else {
+		stop("invalid outcome")
+	}
+
+	data <- read.csv("outcome-of-care-measures.csv", 
+					 na.strings = "Not Available",
+					 stringsAsFactors = FALSE)
+					 
+	data.state <- data[data$State == state, c(2, 7, outcome.index)]
+	
+	# invalid state							
+	if (nrow(data.state) == 0) { 
+  		stop("invalid state")
+  	}
+
+	answer <- subset(data.state, data.state[[3]] == min(data.state[[3]], na.rm = TRUE))
+	# answer <- data.state[data.state[[3]] == min(data.state[[3]], na.rm = TRUE), 1]	
+
+	return(answer[, 1]) #[1]
+}
