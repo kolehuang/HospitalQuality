c0a2fabbd4f041fb124a2c829d5d968201a8b810
Kole Huang <yjhuang_18@hotmail.com>
2014-06-18 11:09:24 -0400

    Steve's version with na.omit()

--- best.R
+++ best.R
@@ -15,15 +15,16 @@ best <- function(state, outcome) {
 					 na.strings = "Not Available",
 					 stringsAsFactors = FALSE)
 					 
-	data.state <- data[data$State == state, c(2, 7, outcome.index)]
-	
+	# data.state <- data[data$State == state, c(2, 7, outcome.index)]
+	data.state <- na.omit(data[data$State == state, c(2, 7, outcome.index)])
+
 	# invalid state							
 	if (nrow(data.state) == 0) { 
   		stop("invalid state")
   	}
 
-	answer <- subset(data.state, data.state[[3]] == min(data.state[[3]], na.rm = TRUE))
-	# answer <- data.state[data.state[[3]] == min(data.state[[3]], na.rm = TRUE), 1]	
+	# answer <- subset(data.state, data.state[[3]] == min(data.state[[3]], na.rm = TRUE))
+	answer <- data.state[data.state[[3]] == min(data.state[[3]]), 1]	
 
-	return(answer[1, 1]) 
+	return(answer[1]) 
 }
